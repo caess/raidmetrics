6b9583bcf66611adbd70597f5cc05b7fc36838ea
Chris Ess <caess@ithiriel.com>
2009-09-16 01:42:02 -0400

    No longer need ActiveRecord caching.

--- bin/import_log.rb
+++ bin/import_log.rb
@@ -3,7 +3,5 @@ filename = ARGV[ 0 ]
 importer = LogFileImporter.new
 
 File.open( filename, 'r' ) do |file|
-  ActiveRecord::Base.cache do
-    importer.import( file )
-  end
+  importer.import( file )
 end
